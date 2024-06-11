58e83250579bdead9b89041e0122132141690764
bburwinkel <burwinkelben1@gmail.com>
2024-06-11 10:06:23 -0400

    create main py and started car_manager py

---
+++ car_manager.py
@@ -0,0 +1,9 @@
+# Grab imports
+from turtle import Turtle
+import random
+
+# Initialize constants
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
